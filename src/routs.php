2828fa8ab672d546ff286d490a067052e0dbfe40
Mahdi Aslami <mahdyaslami@gmail.com>
2020-01-23 13:52:08 +0330

    Update Medoo, Solve optional  SLASH problem.

--- src/routs.php
+++ src/routs.php
@@ -2,6 +2,6 @@
 
 use App\Controller\TableAndViewController;
 
-$app->get('/{table}', TableAndViewController::class . ':getAll');
-$app->get('/{table}/{id:[0-9]+}', TableAndViewController::class . ':getById');
-$app->put('/{table}', TableAndViewController::class . ':insert');
+$app->get('/{table}[/]', TableAndViewController::class . ':getAll');
+$app->get('/{table}/{id:[0-9]+}[/]', TableAndViewController::class . ':getById');
+$app->put('/{table}[/]', TableAndViewController::class . ':insert');

ee259a98b98b1f8908f3ef58a5c0a9a7682fbe85
unknown <Mahdi.Aslami@PART.LOCAL>
2020-07-18 14:19:04 +0430

    install medoo

--- index.php
+++ index.php
@@ -6,6 +6,17 @@ require_once(__DIR__ . '/includes/global-variables.php');
 require_once(__DIR__ . '/includes/func-router.php');
 require_once('vendor/autoload.php');
 
+//
+// Database
+//
+use Medoo\Medoo;
+
+$db = new Medoo($env['connection']);
+
+//
+// Execute project.
+//
+
 try {
     routeRequest($routes, $request);
 } catch (Throwable $e) {
